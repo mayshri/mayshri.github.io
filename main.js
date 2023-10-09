6c48dde3b6dc152647152db80cb0f6af6ca48b0b
Mayank Shrivastava <102333236+mayshri@users.noreply.github.com>
2023-10-09 15:00:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 // Function to show project details
 function showProjectDetails(projectName) {
-  alert(`You clicked on ${projectName}`);
+  const modal = document.getElementById("myModal");
+  const span = document.getElementsByClassName("close")[0];
+  document.getElementById("modalText").innerText = `You clicked on ${projectName}`;
+  modal.style.display = "block";
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
 }
 
-// Contact Form Submission
+// Smooth scrolling for anchor tags
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Contact form
 document.getElementById("contact-form").addEventListener("submit", function(event) {
   event.preventDefault();
   alert("Your message has been sent!");
